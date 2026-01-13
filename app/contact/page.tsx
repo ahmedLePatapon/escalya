4834807875faa29834bf70ca63453e7c6b8adf38
ahmed <ahmed.benlebna@gmail.com>
2026-01-13 11:53:19 +0100

    feat(nextjs-home-page): add placeholder routes

---
+++ app/contact/page.tsx
@@ -0,0 +1,8 @@
+export default function Page() {
+    return (
+        <div className="py-20 text-center">
+            <h1 className="text-3xl font-bold">Contact</h1>
+            <p>En construction...</p>
+        </div>
+    );
+}
